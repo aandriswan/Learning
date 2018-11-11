5594035be674369a990ce06ea6acefd5cc7465a6
aandriswan <37573692+aandriswan@users.noreply.github.com>
2018-11-11 18:11:50 +0700

    Update and rename a.c to FunnyString.c

---
+++ Minggu 2/HackerRank/FunnyString.c
@@ -0,0 +1,107 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+char* readline();
+
+// Complete the funnyString function below.
+
+// Please either make the string static or allocate on the heap. For example,
+// static char str[] = "hello world";
+// return str;
+//
+// OR
+//
+// char* str = "hello world";
+// return str;
+//
+char* funnyString(char* s) {
+    int count = 0;
+    int c = strlen(s)-1;
+    char* str;
+    int i;
+    for (i=0;i<strlen(s);i++)
+    {
+        if ( abs(s[i+1]-s[i]) == abs(s[c]-s[c-1]) )
+        {
+            count += 1;
+            c--;
+        }
+    }
+    
+    if ( count == strlen(s)-1)
+    {
+            str = "Funny";
+    }
+    else{
+    str = "Not Funny";
+            
+        
+    }
+    
+    return str;
+   
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* q_endptr;
+    char* q_str = readline();
+    int q = strtol(q_str, &q_endptr, 10);
+
+    if (q_endptr == q_str || *q_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        char* s = readline();
+
+        char* result = funnyString(s);
+
+        fprintf(fptr, "%s\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
